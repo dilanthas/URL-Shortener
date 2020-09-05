ce486d323d4607ead2e93468cffc34a00db0e75b
Dilantha Silva <dilantha.tyrant@gmail.com>
2020-09-05 20:50:25 +0200

    modifications to readme and postman collection

--- src/main/java/com/urlshortener/repository/AppProperties.java
+++
@@ -1,7 +0,0 @@
-package com.urlshortener.repository;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties("app")
-public class AppProperties {
-}
